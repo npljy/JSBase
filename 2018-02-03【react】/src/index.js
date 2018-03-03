49da002b09005d4103e2de39519c6d2555194df1
NiePeng <niepengsmile@gmail.com>
2018-03-03 10:23:30 +0800

    react练习

--- 2018-02-03【react】/src/index.js
+++ 2018-02-03【react】/src/index.js
@@ -1,25 +1,16 @@
-/*************import****************/
-// import {fn} from './es6_import/import';
-// import fn from './es6_import/import';
-// import './index.css';
-// let num = 10;
-// console.log(num + fn()); //11
-
-/***********react*****************/
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import App from './reactDemo/app/app';
-// import App from './reactDemo/app2/app';
-// import App from './reactDemo/app3/app';
-// import App from './reactDemo/app4/app2';
-// import App from './reactDemo/app5/app';
-import {Index as App}  from './todolist/Index';
-
-ReactDOM.render(<App />,
-  document.getElementById('root')
-);
-
-
+import React from "react"
+import ReactDOM from "react-dom"
+import App from  './test/App3';
+// 平滑过渡
 if(module.hot){
-  module.hot.accept();
+    module.hot.accept();
 }
+
+
+ReactDOM.render(
+    <App />,
+    document.getElementById("root"),
+    // function(){
+    //     console.log("success")
+    // }
+)
